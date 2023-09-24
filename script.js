2abc3e962fde66c531ee5c187e4d31c0b93b062a
Scott Clerge <clergescott03@gmail.com>
2023-09-24 04:39:10 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var firstName = "Scott ";
+var lastName = "Clerge";
+var myAge = 20;
+
+Name= "My name is "+ firstName + lastName
+alert(Name);
+
+Age= "I am " + myAge + " years old"
+alert(Age)
+
+console.log("This class is very interesting")
